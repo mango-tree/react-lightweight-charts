37b864335c5c2dfe67d817100990c11aa59ecabc
Mingyu Kim <contact@mingyu.kim>
2020-02-29 01:14:37 +0900

    Add typing

--- src/react-lightweight-charts/lightweight.tsx
+++ src/react-lightweight-charts/lightweight.tsx
@@ -1,6 +1,8 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from 'react';
 
-import { createChart } from "lightweight-charts";
+import {
+  createChart, PriceLineSource, LineStyle
+} from 'lightweight-charts';
 
 
 export interface ChartData {
@@ -8,13 +10,51 @@ export interface ChartData {
   value: number;
 }
 
-interface lightweightProps {
+export interface PriceFormat {
+  type: 'price' | 'volume' | 'percent' | 'custom';
+  precision: number;
+  minMove: number;
+  formatter: (arg: 'price' | 'volume' | 'percent' | 'custom') => string[] | undefined;
+}
+
+export interface TimeScale {
+  rightOffset: number;
+  barSpacing: number;
+  fixLeftEdge: boolean;
+  lockVisibleTimeRangeOnResize: boolean;
+  rightBarStaysOnScroll: boolean;
+  borderVisible: boolean;
+  borderColor: string;
+  visible: boolean;
+  timeVisible: boolean;
+  secondsVisible: boolean;
+}
+
+export interface LightweightProps {
   data: ChartData[];
   width: number;
   height: number;
+  series: 'line' | 'bar' | 'area';
+  overlay: boolean;
+  title: string;
+  scaleMargins: {
+    top: number;
+    bottom: number;
+  } | undefined;
+  priceLineVisible:	boolean;
+  priceLineSource: PriceLineSource;
+  priceLineColor: string;
+  priceLineStyle: LineStyle;
+  lastValueVisible: boolean;
+  baseLineVisible: boolean;
+  baseLineColor: string;
+  baseLineWidth: number;
+  baseLineStyle: LineStyle;
+  priceFormat: PriceFormat;
+  timeScale: TimeScale;
 }
 
-const Lightweight: React.FC<lightweightProps> = (props) => {
+const Lightweight: React.FC<LightweightProps> = (props) => {
   const { data, width, height } = props;
   const chartRef = useRef<any | null>(null);
   const [lines, setLines] = useState<any | null>(null);
